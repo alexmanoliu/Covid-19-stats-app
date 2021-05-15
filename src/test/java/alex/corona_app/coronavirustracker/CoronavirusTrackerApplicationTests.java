234bc9d84c172c0d9017a2cc2cf724ae314bb5f9
Alex Manoliu <alexandru.manoliu@gmail.com>
2021-05-15 15:19:46 +0200

    finished demo app

---
+++ src/test/java/alex/corona_app/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.corona_app.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
